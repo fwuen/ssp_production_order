56bb6d24a5b0b32d5c836a84d6454c97535a6da8
fwuen <git@fwuen.de>
2018-04-23 08:16:52 +0200

    XmlImporter begonnen

---
+++ src/main/java/data/xml/XmlImporter.java
@@ -0,0 +1,11 @@
+package data.xml;
+
+import java.io.File;
+
+public class XmlImporter {
+    private File xmlFileToImport;
+
+    public XmlImporter(File xmlFileToImport) {
+        this.xmlFileToImport = xmlFileToImport;
+    }
+}
